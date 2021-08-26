94e651cac61b1375a81b03550f606921fee6a956
Leland Mckibben <lmckibben28@hotmail.com>
2021-08-26 21:28:35 +0000

    implemented fetchCpprdsByIP

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //const { fetchMyIP } = require('./iss');
+//const { fetchCoordsByIP } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -8,3 +9,12 @@
 
 //   console.log('It worked! Returned IP:' , ip);
 //});
+
+// fetchCoordsByIP('209.121.229.15', (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned coordinates:' , coordinates);
+// });
--- iss.js
+++ iss.js
@@ -15,9 +15,29 @@ const fetchMyIP = function(callback) {
     body = JSON.parse(body);
     const ipAddress = body.ip;
     callback(error, ipAddress);
-
-    
   });
 };
 
-module.exports = { fetchMyIP };
+
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    };
+    body = JSON.parse(body);
+    const coordinates = {
+      latitude: body.latitude,
+      longitude: body.longitude
+    };
+    callback(null, coordinates);
+  });
+};
+
+module.exports = { fetchCoordsByIP };
