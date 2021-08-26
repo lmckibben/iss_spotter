a778e8b6209280a7b821b0f727d7c89b81808436
Leland Mckibben <lmckibben28@hotmail.com>
2021-08-26 21:07:25 +0000

    implemented fetchMyIP

---
+++ iss.js
@@ -0,0 +1,23 @@
+const request = require("request");
+
+const fetchMyIP = function(callback) {
+  request("https://api.ipify.org/?format=json", (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    body = JSON.parse(body);
+    const ipAddress = body.ip;
+    callback(error, ipAddress);
+
+    
+  });
+};
+
+module.exports = { fetchMyIP };
